f0289169fd4371b4f4a384b0b4e35b75ab2ae0a9
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2021-10-16 02:39:01 +0300

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,35 @@ import './header.scss';
 import React from 'react';
 
 import Logo from '../../assets/svg/logo.svg';
+import Plus from '../../assets/svg/plus.svg';
+import User from '../../assets/svg/user.svg';
+import Button from '../Button/Button';
 
 function Header() {
   return (
-    <div className="headers">
+    <div className="header">
       <div className="header__logo">
         <img src={Logo} alt="logo" />
       </div>
       <div className="header__buttons">
-        <button type="submit">Hesabım</button>
-        <button type="submit">Hesabım</button>
+        <Button
+          type="submit"
+          theme="secondary"
+          size="medium"
+          className="styledButton"
+        >
+          <img src={Plus} alt="plus-svg" />
+          Ürün Ekle
+        </Button>
+        <Button
+          type="submit"
+          theme="secondary"
+          size="medium"
+          className="styledButton"
+        >
+          <img src={User} alt="plus-svg" />
+          Hesabım
+        </Button>
       </div>
     </div>
   );
