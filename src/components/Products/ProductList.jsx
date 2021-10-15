ad9c94f9d3a82f9662c53f4cdf7f3514ea2f1648
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2021-10-16 02:39:38 +0300

    products added

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,6 +1,5 @@
 import './product.scss';
 
-import Header from 'components/Header';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -22,14 +21,24 @@ const ProductsList = () => {
 
   return (
     <div>
-      <Header />
-      <h1>ProductList</h1>
       <div className="product-list">
         {products.productsData.length > 0 &&
           products.productsData.map((item) => (
-            <div className="productList" key={item.id}>
-              <Link to={`/product/${item.id}`}> {item.title}</Link>
-            </div>
+            <Link to={`/product/${item.id}`}>
+              <div className="productList" key={item.id}>
+                <div className="productImg">
+                  <img src={item.imageUrl} alt={item.title} />
+                </div>
+                <div className="productDetail">
+                  <h5 className="productBrand">{item.brand.title}</h5>
+                  <h5 className="productColor">
+                    Renk:
+                    <h6 className="hColor">{item.color.title}</h6>
+                  </h5>
+                </div>
+                <h5 className="productPrice">{item.price} TL</h5>
+              </div>
+            </Link>
           ))}
       </div>
     </div>
