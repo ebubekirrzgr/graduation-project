7371a7c558483b968c82b8d76823ba0dc88a825b
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2021-10-23 02:45:47 +0300

    Accept Offer added

---
+++ src/actions/acceptOffer.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import ACCEPT_OFFERS from 'constants/acceptOffers';
+
+const fetchSuccess = (data) => ({
+  type: ACCEPT_OFFERS.ACCEPT_OFFERS_SUCCESS,
+  payload: data,
+});
+
+const fetchError = (error) => ({
+  type: ACCEPT_OFFERS.ACCEPT_OFFERS_ERROR,
+  payload: error,
+});
+
+const fetchPending = () => ({
+  type: ACCEPT_OFFERS.ACCEPT_OFFERS_PENDING,
+});
+
+const acceptOffers = (offerId) => async (dispatch) => {
+  dispatch(fetchPending());
+  return axios
+    .put(
+      `https://bootcampapi.techcs.io/api/fe/v1/account/accept-offer/${offerId}`,
+      null,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }
+    )
+    .then((data) => dispatch(fetchSuccess(data)))
+    .catch((error) => dispatch(fetchError(error)));
+};
+
+export default acceptOffers;
---
+++ src/reducers/acceptOffers.js
@@ -0,0 +1,37 @@
+import ACCEPT_OFFERS from 'constants/acceptOffers';
+
+const initialState = {
+  acceptOffersData: {},
+  isFetching: false,
+  isError: false,
+};
+
+const acceptOffersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACCEPT_OFFERS.ACCEPT_OFFERS_PENDING:
+      return {
+        ...state,
+        isFetching: true,
+        acceptOffersData: {},
+        isError: false,
+      };
+    case ACCEPT_OFFERS.ACCEPT_OFFERS_SUCCESS:
+      return {
+        ...state,
+        acceptOffersData: action.payload,
+        isFetching: false,
+        isError: false,
+      };
+    case ACCEPT_OFFERS.ACCEPT_OFFERS_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        acceptOffersData: {},
+        isError: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default acceptOffersReducer;
